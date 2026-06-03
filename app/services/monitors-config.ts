9a59e5c686c1633df08506c2d20752e352e9bdff
KaiqueFj <kaiquelferraz@hotmail.com>
2026-06-03 12:36:12 -0300

    feat: removed SSL monitors from view

--- app/services/monitors-config.ts
+++ app/services/monitors-config.ts
@@ -6,7 +6,7 @@ const SECTIONS_CORP: SectionDefinition[] = [
     services: [
       {
         service: "JD Edwards",
-        title: "JDE (Em construção)",
+        title: "JDE ",
         query: '(priority:(p1 OR p2) AND jde AND -ssl tag:("dashboard:site"))',
         dashboardLink:
           "https://p.datadoghq.com/sb/e6ba99a2-3062-11ed-84b0-da7ad0900002-d02b8325d79af5d4d91e7f9caff3e823?fromUser=false&refresh_mode=sliding&tpl_var_Endpoint%5B0%5D=%2A&from_ts=1775122750377&to_ts=1775137150377&live=true",
